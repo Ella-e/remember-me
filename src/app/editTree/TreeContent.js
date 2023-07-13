ac2db80aa801d164c2511cc00582a88ef5163d82
Ella-e <mzyu_bj@163.com>
2023-07-13 18:22:55 +0800

    fix scrollbar and fix some document problem

--- src/app/editTree/TreeContent.js
+++ src/app/editTree/TreeContent.js
@@ -39,7 +39,10 @@ import { auth, db } from "../firebase-config";
 import ReactQuill from "react-quill";
 import "../../../node_modules/react-quill/dist/quill.snow.css";
 import { FORMATS, MODULES } from "./RichText";
-import { Scrollbars } from "react-custom-scrollbars";
+// import { Scrollbars } from "react-custom-scrollbars";
+// import { Scrollbars } from "rc-scrollbars";
+// import * as Scroll from "react-scroll";
+// import { Element } from "react-scroll";
 import "./page.css";
 import { useRouter, useSearchParams } from "next/navigation";
 
@@ -441,83 +444,81 @@ const TreeContent = () => {
       )}
       {editNode && (
         <div className="border-line-[#E4E6F0] rounded-2xl leading-5 overflow-auto h-[189px] mb-[26px]">
-          <Scrollbars style={{ width: 1000, height: 650 }}>
-            <Container maxWidth="sm">
-              <h1>
-                First Name<i style={{ color: "red" }}>*</i>
-              </h1>
-              <Input.TextArea
-                value={firstName}
-                className="px-4 py-2 outline-none resize-none !h-full !border-none flex"
-                onChange={(e) => setFirstName(e.target.value)}
+          <Container maxWidth="sm">
+            <h1>
+              First Name<i style={{ color: "red" }}>*</i>
+            </h1>
+            <Input.TextArea
+              value={firstName}
+              className="px-4 py-2 outline-none resize-none !h-full !border-none flex"
+              onChange={(e) => setFirstName(e.target.value)}
               // placeholder=""
-              ></Input.TextArea>
-              <h1>
-                Last Name<i style={{ color: "red" }}>*</i>
-              </h1>
-              <Input.TextArea
-                value={lastName}
-                className="px-4 py-2 outline-none resize-none !h-full !border-none flex"
-                onChange={(e) => setLastName(e.target.value)}
+            ></Input.TextArea>
+            <h1>
+              Last Name<i style={{ color: "red" }}>*</i>
+            </h1>
+            <Input.TextArea
+              value={lastName}
+              className="px-4 py-2 outline-none resize-none !h-full !border-none flex"
+              onChange={(e) => setLastName(e.target.value)}
               // placeholder=""
-              ></Input.TextArea>
-              <h1>Nick Name</h1>
-              <Input.TextArea
-                value={nickName}
-                className="px-4 py-2 outline-none resize-none !h-full !border-none flex"
-                onChange={(e) => setNickName(e.target.value)}
+            ></Input.TextArea>
+            <h1>Nick Name</h1>
+            <Input.TextArea
+              value={nickName}
+              className="px-4 py-2 outline-none resize-none !h-full !border-none flex"
+              onChange={(e) => setNickName(e.target.value)}
               // placeholder=""
-              ></Input.TextArea>
-              <h1>Gender</h1>
-              <Select
-                className="px-4 py-2 outline-none resize-none !h-full !border-none flex"
-                value={gender}
+            ></Input.TextArea>
+            <h1>Gender</h1>
+            <Select
+              className="px-4 py-2 outline-none resize-none !h-full !border-none flex"
+              value={gender}
+              onChange={(e) => {
+                setGender(e.target.value);
+                setOtherGender("");
+              }}
+            >
+              <MenuItem value={"male"}>male</MenuItem>
+              <MenuItem value={"female"}>female</MenuItem>
+              <MenuItem value={"other"}>other</MenuItem>
+            </Select>
+            {gender === "other" && (
+              <TextField
+                placeholder="please specify"
+                value={otherGender}
                 onChange={(e) => {
-                  setGender(e.target.value);
-                  setOtherGender("");
+                  setOtherGender(e.target.value);
                 }}
-              >
-                <MenuItem value={"male"}>male</MenuItem>
-                <MenuItem value={"female"}>female</MenuItem>
-                <MenuItem value={"other"}>other</MenuItem>
-              </Select>
-              {gender === "other" && (
-                <TextField
-                  placeholder="please specify"
-                  value={otherGender}
-                  onChange={(e) => {
-                    setOtherGender(e.target.value);
-                  }}
-                ></TextField>
-              )}
-              <h1>Status</h1>
-              <Select
-                className="px-4 py-2 outline-none resize-none !h-full !border-none flex"
-                value={status}
-                onChange={(e) => setStatus(e.target.value)}
-              >
-                <MenuItem value={"alive"}>alive</MenuItem>
-                <MenuItem value={"desceased"}>deceased</MenuItem>
-              </Select>
-              <div className="">
-                <h1>Life Story</h1>
-                <ReactQuill
-                  modules={MODULES}
-                  formats={FORMATS}
-                  value={richTextValue}
-                  onChange={setRichTextValue}
-                  theme="snow"
-                />
-              </div>
-              <Button
-                type="submit"
-                onClick={isEdit ? handleSaveEditMember : handleAddMember}
-              >
-                Save Member
-              </Button>
-              <Button onClick={() => handleCancel("add")}>Cancel</Button>
-            </Container>
-          </Scrollbars>
+              ></TextField>
+            )}
+            <h1>Status</h1>
+            <Select
+              className="px-4 py-2 outline-none resize-none !h-full !border-none flex"
+              value={status}
+              onChange={(e) => setStatus(e.target.value)}
+            >
+              <MenuItem value={"alive"}>alive</MenuItem>
+              <MenuItem value={"desceased"}>deceased</MenuItem>
+            </Select>
+            <div className="">
+              <h1>Life Story</h1>
+              <ReactQuill
+                modules={MODULES}
+                formats={FORMATS}
+                value={richTextValue}
+                onChange={setRichTextValue}
+                theme="snow"
+              />
+            </div>
+            <Button
+              type="submit"
+              onClick={isEdit ? handleSaveEditMember : handleAddMember}
+            >
+              Save Member
+            </Button>
+            <Button onClick={() => handleCancel("add")}>Cancel</Button>
+          </Container>
         </div>
       )}
     </div>
--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import reactDom from "react-dom";
-import Home from "./page";
-import { BrowserRouter } from "react-router-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import reactDom from "react-dom";
+// import Home from "./page";
+// import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Home></Home>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-// ReactDOM.render(<p >Hello World</p>, document.getElementById('root'));
+// const root = ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <Home></Home>
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+// // ReactDOM.render(<p >Hello World</p>, document.getElementById('root'));
